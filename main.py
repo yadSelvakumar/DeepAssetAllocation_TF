ef678f017731b2448cfa3a41ea752b57717a42e6
yatheesan selvakumar <yatheesan.selvakumar@PCRES01.chesterfield.local>
2023-06-15 03:42:31 +0100

    fixed_horizon_mat

--- main.py
+++ main.py
@@ -39,4 +39,4 @@ if __name__ == '__main__':
     from src.calc_weights import calc_fixed_horizon_allocations,calc_term_fund_allocations
     # train_model(args)
     alphas_fixed_horizon = calc_fixed_horizon_allocations(args,invest_horizon = 47)
-    calc_term_fund_allocations(args,invest_horizon = 119)
+    # calc_term_fund_allocations(args,invest_horizon = 119)
